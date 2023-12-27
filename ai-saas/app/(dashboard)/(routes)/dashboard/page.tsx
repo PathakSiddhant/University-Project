c3c92cc1b7351b633b803005d8723f3b6741bbc7
Rohit Murmu <rohitmurmu038@gmail.com>
2023-12-28 01:27:40 +0530

    folder setup

---
+++ ai-saas/app/(dashboard)/(routes)/dashboard/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from '@/components/ui/button'
+import Image from 'next/image'
+
+const DashboardPage = () => {
+  return (
+    <Button variant="destructive">click me</Button>
+  )
+}
+
+export default DashboardPage;
