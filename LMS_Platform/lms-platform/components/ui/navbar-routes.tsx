f397c8a93d8d8ac2b2b5a09a3ab8f553632a9fa5
vidyarthinikhil09 <vidyarthinikhil2@gmail.com>
2023-12-28 22:17:14 +0530

    Created Layout

---
+++ LMS_Platform/lms-platform/components/ui/navbar-routes.tsx
@@ -0,0 +1,11 @@
+"user clients";
+
+import { UserButton } from "@clerk/nextjs";
+
+export const NavbarRoutes = () => {
+    return (
+        <div className="flex gap-x-2 ml-auto">
+          <UserButton />
+        </div>
+    )
+}
