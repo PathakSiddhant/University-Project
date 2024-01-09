4a844c1e3776840107d50db97e7a1cc9c3a699a5
vidyarthinikhil09 <vidyarthinikhil2@gmail.com>
2024-01-09 17:05:47 +0530

    Created Edit Page UI(Chapter Description form, chapter Access Form)

--- LMS_Platform/lms-platform/app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
+++ LMS_Platform/lms-platform/app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -10,6 +10,7 @@ import { CategoryForm } from "./_components/category-form";
 import { Label } from "@radix-ui/react-label";
 import { PriceForm } from "./_components/price-form";
 import { AttachmentForm } from "./_components/attachment-form";
+import { ChaptersForm } from "./_components/chapters-form";
 
 const CourseIdPage = async ({
     params
@@ -24,16 +25,22 @@ const CourseIdPage = async ({
 
     const course = await db.course.findUnique({
         where: {
-            id: params.courseId,
+          id: params.courseId,
+          userId
         },
         include: {
-            attachments: {
-                orderBy: {
-                    createdAt: "desc",
-                },
+          chapters: {
+            orderBy: {
+              position: "asc",
             },
+          },
+          attachments: {
+            orderBy: {
+              createdAt: "desc",
+            },
+          },
         },
-    });
+      });
 
     const categories = await db.category.findMany({
         orderBy: {
@@ -54,6 +61,7 @@ const CourseIdPage = async ({
         course.imageUrl,
         course.price,
         course.categoryId,
+        course.chapters.some(chapter => chapter.isPublished),
     ];
 
     const totalFields = requiredFields.length;
@@ -113,10 +121,11 @@ const CourseIdPage = async ({
                                 Course chapters
                             </h2>
                         </div>
-                        <div>
-                            TODO: Chapters
-                        </div>
-                    </div>
+              <ChaptersForm
+                initialData={course}
+                courseId={course.id}
+              />
+            </div>
                     <div>
                         <div className="flex items-center gap-x-2">
                             <IconBadge icon={CircleDollarSign} />
