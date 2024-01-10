a683b83f3570ed51bf80c062f82a96cdd1d997a5
vidyarthinikhil09 <vidyarthinikhil2@gmail.com>
2024-01-10 15:56:20 +0530

    MUX Setup & Chapter Video Form

--- LMS_Platform/lms-platform/app/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/[chapterId]/page.tsx
+++ LMS_Platform/lms-platform/app/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/[chapterId]/page.tsx
@@ -11,6 +11,7 @@ import { ChapterDescriptionForm } from "./_components/chapter-description-form";
 
 import { ChapterTitleForm } from "./_components/chapter-title-form";
 import { ChapterAccessForm } from "./_components/chapter-access-form";
+import { ChapterVideoForm } from "./_components/chapter-video-form";
 
 const ChapterIdPage = async ({
   params
@@ -113,6 +114,11 @@ const ChapterIdPage = async ({
                 Add a video
               </h2>
             </div>
+            <ChapterVideoForm
+              initialData={chapter}
+              chapterId={params.chapterId}
+              courseId={params.courseId}
+            />
         </div>
       </div>
     </div>
